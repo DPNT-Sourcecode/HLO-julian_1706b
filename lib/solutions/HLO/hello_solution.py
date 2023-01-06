d26805c582398e67376ab838bcddbbd6c4175c61
sbx_user1051 <sbx_user1051@169.254.30.69>
2023-01-06 20:09:07 +0000

    Fri Jan 06 20:09:07 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,4 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, "+friend_name+"!"
+    raise NotImplementedError()
--- test/solution_tests/HLO/test_hlo.py
+++
@@ -1,6 +0,0 @@
-from solutions.HLO import hello_solution
-
-
-class TestHello():
-    def test_hello(self):
-        assert hello_solution.hello("x") == "Hello, World!"
